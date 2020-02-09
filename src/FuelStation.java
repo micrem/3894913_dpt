871dee64e91104b2f48e85ca84b5841c2874e5ef
michael.rempel@gmx.net <michael.rempel@gmx.net>
2020-02-10 00:03:42 +0100

    implementation WaitingArea

--- src/FuelStation.java
+++ src/FuelStation.java
@@ -4,4 +4,46 @@ public class FuelStation {
 	private StationEntrance entranceTruck;
 	private WaitingArea waitingArea;
 
+    public FuelStation(boolean initialze) {
+        if (initialze) init();
+    }
+
+    public FuelStation() {
+    }
+
+    public StationEntrance getEntranceCar() {
+        return entranceCar;
+    }
+
+    public void setEntranceCar(StationEntrance entranceCar) {
+        this.entranceCar = entranceCar;
+    }
+
+    public StationEntrance getEntranceTruck() {
+        return entranceTruck;
+    }
+
+    public void setEntranceTruck(StationEntrance entranceTruck) {
+        this.entranceTruck = entranceTruck;
+    }
+
+    public WaitingArea getWaitingArea() {
+        return waitingArea;
+    }
+
+    public void setWaitingArea(WaitingArea waitingArea) {
+        this.waitingArea = waitingArea;
+    }
+
+    private void init(){
+        if (entranceCar == null) {
+            entranceCar = new StationEntrance();
+        }
+        if (entranceTruck == null) {
+            entranceTruck = new StationEntrance();
+        }
+        if (waitingArea == null) {
+            waitingArea = new WaitingArea();
+        }
+    }
 }
--- src/WaitingArea.java
+++ src/WaitingArea.java
@@ -1,6 +1,47 @@
+import java.util.Map;
+
 public class WaitingArea {
 
-	private ParkingSpace[] carParkingSpaces;
-	private ParkingSpace[] truckParkingSpaces;
+	private ParkingSpace[] carParkingSpaces = new ParkingSpace[15];
+	private ParkingSpace[] truckParkingSpaces = new ParkingSpace[35];
 
+	public WaitingArea() {
+		for (int i = 0; i < carParkingSpaces.length; i++) {
+			int j=i+1;
+			carParkingSpaces[i] = new ParkingSpace( "P"+(j>9?j:("0"+j)) );
+		}
+		for (int i = 0; i < truckParkingSpaces.length; i++) {
+			int j=i+16;
+			truckParkingSpaces[i] = new ParkingSpace("P"+(j>9?j:("0"+j)));
+		}
+	}
+
+	public ParkingSpace getParkingSpace(int index) {
+		return index<15?carParkingSpaces[index]:truckParkingSpaces[index-15];
+	}
+
+	public ParkingSpace getFreeCarParkingSpace(){
+		for (int i = 0; i < carParkingSpaces.length; i++) {
+			if(carParkingSpaces[i].getVehicle()==null) return carParkingSpaces[i];
+		}
+		return null;
+	}
+
+	public ParkingSpace getFreeTruckParkingSpace(){
+		for (int i = 0; i < truckParkingSpaces.length; i++) {
+			if(truckParkingSpaces[i].getVehicle()==null) return truckParkingSpaces[i];
+		}
+		return null;
+	}
+
+	public Vehicle getWaitingVehicle(){
+		for (int i = 0; i < 50; i++) {
+			if (getParkingSpace(i)!=null) {
+				Vehicle vehicle = getParkingSpace(i).getVehicle();
+				getParkingSpace(i).setVehicle(null);
+				return vehicle;
+			}
+		}
+		return null;
+	}
 }
