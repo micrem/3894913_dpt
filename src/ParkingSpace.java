65db919ad2f60d120055f386675317263bfec076
michael.rempel@gmx.net <michael.rempel@gmx.net>
2020-02-10 00:04:18 +0100

    implement ParkingSpace

--- src/ParkingSpace.java
+++ src/ParkingSpace.java
@@ -1,6 +1,21 @@
 public class ParkingSpace {
 
 	private Vehicle vehicle;
-	private string id;
+	private String id;
 
+	public ParkingSpace(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
 }
